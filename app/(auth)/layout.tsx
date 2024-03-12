832b85c85ca58921ee3e4ba823097b6839b9eda5
Valentyn <valentunpron@gmail.com>
2024-03-13 00:07:20 +0200

    auth window

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google";
+
+import '../globals.css'
+import Providers from "../(root)/provider";
+
+export const metadata = {
+    title: 'TodoDash',
+    description: 'A Next.js 14 Meta Knot',
+}
+
+const inter = Inter({ subsets: ['latin']});
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <ClerkProvider>
+        <html lang="ea">
+          <body className={`${inter.className} h-screen py-5 flex justify-center items-center`}>
+            <Providers>
+             {children}
+            </Providers>
+            </body>
+        </html>
+      </ClerkProvider>
+    )
+  }
